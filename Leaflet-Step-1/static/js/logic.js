9cf0677484006072c39482b1f4901e7d155b6860
Karla-Flores <ms.kaflores@gmail.com>
2021-08-03 14:55:21 -0500

    Updating pop up

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,21 +4,21 @@ let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.ge
 // Get marker color bases on earthquake magnitude
 function getColor(mag) {
     if (mag >= 5) {
-        return "#C73866"
+        return '#C73866'
     } else {
         if (mag > 4) {
-            return "#FE676e"
+            return '#FE676e'
         } else {
             if (mag > 3) {
-                return "#FD8F52"
+                return '#FD8F52'
             } else {
                 if (mag > 2) {
-                    return "#FFBD71"
+                    return '#FFBD71'
                 } else {
                     if (mag > 1) {
-                        return "#FFDCA2"
+                        return '#FFDCA2'
                     } else {
-                        return "#C8C6C9"
+                        return '#C8C6C9'
                     }
                 }
             }
@@ -27,32 +27,12 @@ function getColor(mag) {
 };
 
 // Function for map features
-function createFeature(data){
+function createFeature(data) {
     //Pop up layer using title, title and magnitude
-    function onEachFeature (feature, layer){
-        layer.binfPopup('<h1>'+ 'feature.title' + '</h1>' + '<hr>' )
-    }
+    function onEachFeature(feature, layer) {
+        layer.binfPopup('<h1>' + feature.properties.title + '</h1>' + '<hr>' + '<p>' + 'Type : ' + feature.properties.type + '</p>' + '<br>' + '<p>' + 'Magnitude : ' + feature.properties.mag + '</p>')
+    };
 }
 
 
-// Creating the map object
-let myMap = L.map("map", {
-    center: [34.0522, -118.2437],
-    zoom: 5
-});
 
-// Adding the tile layer
-let layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-// Creating earthquake layerGroup
-let earthquake = L.layerGroup()
-
-
-d3.json(url).then(function (data) {
-    console.log(data);
-    function markerSize(mag) {
-        return mag * 3
-    }
-});
