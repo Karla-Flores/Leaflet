c835d0167c9b2f723eeb3cc0d3aca70b81da6eaf
Karla-Flores <ms.kaflores@gmail.com>
2021-08-03 16:13:27 -0500

    Setting popup

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -12,8 +12,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-
-
 // Get marker color bases on earthquake magnitude
 function getColor(mag) {
     if (mag >= 5) {
@@ -57,49 +55,14 @@ d3.json(url).then(function (data) {
     });
 }).addTo(myMap);
 
+// Starting pop up layers
 function onEachFeature(feature, layer){
-    console.log('Creating pop up'),
+    // console.log('Creating pop up'),
     //Pop up layer using title, title and magnitude
-    layer.binfPopup('<h1>' + feature.properties.title + '</h1>' + '<hr>' + '<p>' + 'Type : ' + feature.properties.type + '</p>' + '<br>' + '<p>' + 'Magnitude : ' + feature.properties.mag + '</p>')
-}
+    var popupText = (layer.binfPopup('<h1>' + 'Location : '+ feature.properties.title + '</h1>' + '<hr>' + '<p>' + 'Type : ' + feature.properties.type + '</p>' + '<br>' + '<p>' + 'Magnitude : ' + feature.properties.mag + '</p>'
+    ))};
 
-// // Function for map features
-// function createFeatures(edata) {
-//     //Pop up layer using title, title and magnitude
-//     function onEachFeature(feature, layer) {
-//         layer.binfPopup('<h1>' + feature.properties.title + '</h1>' + '<hr>' + '<p>' + 'Type : ' + feature.properties.type + '</p>' + '<br>' + '<p>' + 'Magnitude : ' + feature.properties.mag + '</p>')
-//     }
-//     // Creating circle marker
-//     let earthquake = L.geoJson(edata, {
-//         poinToLayer: function (feature, latlng) {
-//             // Defining circle radius according to the magnitude
-//             return new L.CircleMarker(latlng, {
-//                 radius: feature.properties.mag * 5,
-//                 fillOpacity: 0.6,
-//                 color: getColor(feature.properties.mag)
-//             })
-//         },
-//         //Adding pup ups
-//         onEachFeature: onEachFeature
-//     });
-//     createMap(earthquake);
-// });
+layer.bindPopup(popupText);
 
-// // Function for creating map
-// function createMap(earthquake) {
-//     // Tile Layer
-//     let layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     });
-//     // 
-//     let myMap = L.map('map', {
-//         center: [34.0522, -118.2437],
-//         zoom: 5
-//     });
-// }
-
-
-    // Adding the tile layer
-
-
+var legend = L.control({ position: 'bottomright' })
 
