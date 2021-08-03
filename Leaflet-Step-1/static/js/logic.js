9f6b600008ae5b1a59d4fc233b55bb4238fac855
Karla-Flores <ms.kaflores@gmail.com>
2021-08-03 12:58:28 -0500

    Do over - establishing mag scale and stating circles features

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,31 +1,49 @@
+// Load the GeoJSON url.
+let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson';
+
+// Get marker color bases on earthquake magnitude
+function getColor(mag) {
+    if (mag >= 5) {
+        return "#C73866"
+    } else {
+        if (mag > 4) {
+            return "#FE676e"
+        } else {
+            if (mag > 3) {
+                return "#FD8F52"
+            } else {
+                if (mag > 2) {
+                    return "#FFBD71"
+                } else {
+                    if (mag > 2) {
+                        return "#FFDCA2"
+                    } else {
+                        return "#C8C6C9"
+                    }
+                }
+            }
+        }
+    }
+};
+
 // Creating the map object
-var myMap = L.map("map", {
+let myMap = L.map("map", {
     center: [34.0522, -118.2437],
     zoom: 5
 });
 
 // Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Load the GeoJSON url.
-let url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson';
+// Creating earthquake layerGroup
+let earthquake = L.layerGroup()
 
-var geojson;
 
-// To do:
 d3.json(url).then(function (data) {
     console.log(data);
-    L.geoJSON(data, {
-        onEachFeature: onEachFeature,
-        pointToLayer: function (feature, latlng) {
-            console.log('Creatin marker');
-            return null;
-        }
-
-    });
+    function markerSize(mag) {
+        return mag * 3
+    }
 });
-function onEachFeature(feature, layer){
-    console.log('Creating pop up')
-}
